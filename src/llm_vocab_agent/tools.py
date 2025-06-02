c0f9c48c146929af8a40e9f079f77373541ae7be
VladMerk <merkvladimir@gmail.com>
2025-06-02 20:13:33 +0300

    Мелкие исправления

--- src/llm_vocab_agent/tools.py
+++ src/llm_vocab_agent/tools.py
@@ -1,11 +1,9 @@
-from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts import ChatPromptTemplate
 from langchain.tools import tool
 from langchain_core.output_parsers import StrOutputParser
 
 from llm_vocab_agent.llms import llm
-from llm_vocab_agent.models.substantiv import SubstantivList
-from llm_vocab_agent.models.verb import VerbList
+from llm_vocab_agent.models import SubstantivList, VerbList
 
 
 @tool
@@ -13,15 +11,11 @@ async def get_verbs(n: int):
     """
     Возвращает n глаголов немецкого языка
     """
-    parser = PydanticOutputParser(pydantic_object=VerbList)
 
     prompt = ChatPromptTemplate.from_template(
         """Вы очень опытный преподаватель немецкого языка.
         Вам необходимо вернуть {n} глаголов немецкого языка.
         Если глаголы имеют управление предлогом, тогда его нужно указать и объяснить что значит.
-
-        Верни список глаголов строго в формате JSON, где каждый элемент соответствует следующей структуре:
-        {format_instructions}
 
         Примеры:
         sich freuen - (über + Akk) - радоваться тому, что уже есть
@@ -33,7 +27,7 @@ async def get_verbs(n: int):
 
     chain = prompt | llm.with_structured_output(VerbList) | StrOutputParser()
 
-    result = chain.invoke(input={"n": n, "format_instructions": parser.get_format_instructions()})
+    result = chain.invoke(input={"n": n})
 
     return str(result)
 
@@ -43,12 +37,10 @@ async def get_substantiv(n: int):
     """
     Возвращает n существительных немецкого языка
     """
-    # parser = PydanticOutputParser(pydantic_object=SubstantivList)
 
     prompt = ChatPromptTemplate.from_template(
         """Вы очень опытный преподаватель немецкого языка.
         Вам необходимо вернуть {n} существительных немецкого языка.
-
         """
     )
 
@@ -63,7 +55,7 @@ if __name__ == "__main__":
     import asyncio
 
     async def main():
-        result = await get_substantiv.ainvoke({"n": 5})
+        result = await get_verbs.ainvoke({"n": 5})
 
         print(result)
 
